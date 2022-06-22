207d03ce4ad58f53f0689a0cde7c536bc7bf81f8
arkag9 <60177011+arkag9@users.noreply.github.com>
2022-06-22 10:01:50 +0530

    third commit

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup,find_packages
 from typing import List
 
 ##Declaring variables for setup functions 
@@ -15,7 +15,7 @@ def get_requirements_list()->List[str]:
     
 
     with open(REQUIREMENT_FILE_NAME) as requirement_file:
-        return requirement_file.readlines()
+        return requirement_file.readlines().remove("-e .")
 
 
 setup(
@@ -24,6 +24,6 @@ setup(
     version=VERSION,
     author=AUTHOR,
     description=DESCRIPTION,
-    packeges=PACKEGES,
+    packeges=find_packages(),
     install_requires=get_requirements_list()
 )
