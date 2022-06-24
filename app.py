bbb4f37e5a92aca1d83ba0abc21963b4263e56d8
arkag9 <60177011+arkag9@users.noreply.github.com>
2022-06-24 18:33:43 +0530

    logging and exception

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 from flask import Flask
+import sys
+from housing.logger import logging
+from housing.exception import HousingException
 
 app=Flask(__name__)
 
 @app.route("/",methods=['GET','POST'])
 
 def index():
+    try:
+        raise Exception("We are testing our exception")
+    
+    except Exception as e:
+        housing=HousingException(e,sys)
+        logging.info(housing.error_message)
+        logging.info("we are testing our logging module")
     return "CI/CD Pipeline has been established"
 
 if __name__=="__main__":
