5b9b7935c3bf893c3d6d18d0586bfcb1d636eb6f
arkag9 <60177011+arkag9@users.noreply.github.com>
2022-06-24 08:51:43 +0530

    Folder structure completed

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from typing import List
 ##Declaring variables for setup functions 
 
 PROJECT_NAME="housing predictor"
-VERSION="0.0.1"
+VERSION="0.0.2"
 AUTHOR="Arkadev"
 DESCRIPTION="This is the first ML project"
 PACKEGES=["housing"]
